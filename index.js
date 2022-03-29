c2045d56e56ba258beabb81d68c845d996678d9a
Hunter Shaw <hunter.shaw@classyllama.com>
2022-03-29 09:18:20 -0500

    Update

--- index.js
+++ index.js
@@ -12,20 +12,20 @@ server.get('/', async (req, res) => {
     last = req.query
     console.log(req.query)
     res.send('Thumbs up.')
-    if (req.query.code && req.query.context && req.query.scope){
+    if (req.query.code && req.query.context && req.query.scope) {
 
         // This is the step that tells BC to show app within admin section
 
         const request = await fetch(`https://login.bigcommerce.com/oauth2/token?client_id=${client_id}&client_secret=${client_secret}&code=${req.query.code}&scope=${req.query.scope}&grant_type=authorization_code&redirect_uri=${publicURL}&context=${req.query.context}`, {
-            method:'post',
-            headers:{
+            method: 'post',
+            headers: {
                 'content-type': 'application/x-www-form-urlencoded'
             }
         })
 
         console.log('Request sent to BC.', request.status, request.statusText)
     }
-    
+
 })
 
 server.post('/', (req, res) => {
